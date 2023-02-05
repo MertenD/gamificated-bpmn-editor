a25fa8632083c066acce373143b3a178c4cc0bfa
Merten Dieckmann <merten.dieckmann@web.de>
2023-02-05 21:21:31 +0100

    refactor: transform challenges to real bpmn2.0 notation on export

--- src/model/Bpmn.ts
+++ src/model/Bpmn.ts
@@ -4,3 +4,10 @@ export type BpmnDto = {
     nodes: Node[],
     edges: Edge[]
 }
+
+export function mergeBpmnDto(bpmnDtos: BpmnDto[]): BpmnDto {
+    return {
+        nodes: bpmnDtos.flatMap((bpmnDto: BpmnDto) => bpmnDto.nodes),
+        edges: bpmnDtos.flatMap((bpmnDto: BpmnDto) => bpmnDto.edges)
+    } as BpmnDto
+}
