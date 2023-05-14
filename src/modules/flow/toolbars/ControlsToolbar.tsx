f100715ec33239c97ac668a9b52d813ad4a75186
Merten Dieckmann <merten.dieckmann@web.de>
2023-05-14 23:14:35 +0200

    refactor: UI of controls toolbar with better descriptions of buttons

--- src/modules/flow/toolbars/ControlsToolbar.tsx
+++ src/modules/flow/toolbars/ControlsToolbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import useStore from "../../../store";
 import {useReactFlow} from "reactflow";
 import {onExport, onLoad, onSave} from "../../../util/ImportExportUtils";
@@ -9,6 +9,7 @@ export default function ControlsToolbar() {
     const getChildren = useStore((state) => state.getChildren)
     const getNodeById = useStore((state) => state.getNodeById)
     const reactFlowInstance = useReactFlow();
+    const inputFile = useRef<HTMLInputElement | null>(null);
 
     return (
         <aside>
@@ -23,12 +24,27 @@ export default function ControlsToolbar() {
                 justifyContent: 'center',
             }}>
                 <button style={{ width: "100%", marginBottom: 10 }} onClick={_ => onSave(nodes, edges)}>
-                    Save
+                    Save editor file (.json)
                     <a id="downloadSave" style={{ display: "none"}}></a>
                 </button>
-                <input style={{ width: "100%", marginBottom: 10 }} type="file" onChange={event => onLoad(event, reactFlowInstance)} />
+                <button style={{
+                    width: "100%",
+                    marginBottom: 10,
+                    userSelect: "none",
+                }} onClick={() => {
+                    if (inputFile.current !== null) {
+                        inputFile.current.click();
+                    } else {
+                        console.warn("Error while uploading")
+                    }
+                }}>
+                    <input accept={".json"} type='file' id='file' ref={inputFile} hidden onChange={(event) => {
+                        onLoad(event, reactFlowInstance)
+                    }}/>
+                    Upload editor file (.json)
+                </button>
                 <button style={{ width: "100%" }} onClick={_ => onExport(nodes, edges, getChildren, getNodeById)}>
-                    Export for Engine
+                    Export for Engine (.bpmn)
                     <a id="downloadExport" style={{ display: "none"}}></a>
                 </button>
             </div>
