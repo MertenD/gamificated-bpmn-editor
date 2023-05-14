273a3e6819aee7e75dbcb2b3e3edd53d6b604308
Merten Dieckmann <merten.dieckmann@web.de>
2023-05-14 23:16:01 +0200

    feat: disable start node in the nodes toolbar when a start is already placed

--- src/modules/flow/toolbars/NodesToolbar.tsx
+++ src/modules/flow/toolbars/NodesToolbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {activityShapeStyle} from "../nodes/ActivityNode";
 import {startNodeShapeStyle} from "../nodes/StartNode";
 import {GatewayShapeStyle} from "../nodes/GatewayNode";
@@ -7,12 +7,20 @@ import {NodeTypes} from "../../../model/NodeTypes";
 import {challengeShapeStyle} from "../nodes/ChallengeNode";
 import {infoNodeShapeStyle} from "../nodes/InfoNode";
 import {eventShapeStyle} from "../nodes/GamificationEventNode";
+import useStore from "../../../store";
 
 export default function NodesToolbar() {
     const onDragStart = (event: any, nodeType: String, nodeData: any) => {
         event.dataTransfer.setData('application/reactflow', JSON.stringify({ nodeType, nodeData }));
         event.dataTransfer.effectAllowed = 'move';
     };
+
+    const nodes = useStore((state) => state.nodes)
+    const [isStartAlreadyPlaced, setIsStartAlreadyPlaced] = useState<boolean>(false)
+
+    useEffect(() => {
+        setIsStartAlreadyPlaced(nodes.filter(node => node.type === NodeTypes.START_NODE).length !== 0)
+    }, [nodes])
 
     return (
         <aside>
@@ -27,9 +35,12 @@ export default function NodesToolbar() {
                 justifyContent: 'center',
             }}>
                 Start
-                <div draggable style={{ ...startNodeShapeStyle,  marginBottom: 10 }} onDragStart={(event) =>
-                    onDragStart(event, NodeTypes.START_NODE, {})
-                } />
+                <div draggable={!isStartAlreadyPlaced}
+                    style={{ ...startNodeShapeStyle, marginBottom: 10, borderColor: isStartAlreadyPlaced ? "lightgray" : undefined }}
+                    onDragStart={(event) =>
+                        onDragStart(event, NodeTypes.START_NODE, {})
+                    }
+                />
                 End
                 <div draggable style={{ ...endNodeShapeStyle,  marginBottom: 10 }} onDragStart={(event) =>
                     onDragStart(event, NodeTypes.END_NODE, {})
